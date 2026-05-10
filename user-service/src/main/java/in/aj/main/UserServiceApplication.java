bc78777fbbb1e90b8eaf25aaaf732df6ee251fc3
Aashi Jain <aashijain064@gmail.com>
2026-05-10 23:22:41 +0530

    --

--- user-service/src/main/java/in/aj/main/UserServiceApplication.java
+++ user-service/src/main/java/in/aj/main/UserServiceApplication.java
@@ -2,10 +2,11 @@ package in.aj.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
