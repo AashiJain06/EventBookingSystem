1cd2704c32cbec7927147647b3f922d708b66924
Aashi Jain <aashijain064@gmail.com>
2026-05-11 23:37:27 +0530

    Implement config server

--- config-server/src/main/java/in/aj/main/ConfigServerApplication.java
+++ config-server/src/main/java/in/aj/main/ConfigServerApplication.java
@@ -3,16 +3,15 @@ package in.aj.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-
-
-@EnableDiscoveryClient
 @SpringBootApplication
-
-public class UserServiceApplication {
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
